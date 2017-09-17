887b6735749637e8f61ed1662063c12203dbd95a
an-art <anartyomchik@gmail.com>
2017-09-17 20:11:24 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function multiply(first, second) {
-  // your solution
+	var fir = first.split('').reverse().map( function(n) { 
+		return +n;
+	});
+	var sec = second.split('').reverse().map( function(n) { 
+		return +n;
+	});
+	var res = [];
+
+	for (var i=0; i<sec.length; i++) {
+		var prev_result = 0;
+		var fl = fir.length;
+		res[i] = fir.map( function(el) {
+			var mult = sec[i] * el;
+			var rest = mult % 10;
+			var result = Math.floor(mult / 10);
+			var sum = prev_result + rest;
+			fl--;
+			prev_result = result;
+			if (sum>=10 && fl>0) {
+				sum -= 10;
+				prev_result++;
+			}
+
+			return sum;
+		});
+	}
+
+	for (var i=0; i<res.length; i++) {
+		res.map( function(el) {
+			return el.unshift(0);
+		});
+		let x = 0;
+		let j = 0;
+		s[j] = 0;
+		while (x!=res[i].length) {
+			s[j] += res[i][x];
+			x++;
+			console.log(s);
+		}
+		j++;
+	}
+
+	return res;
 }
