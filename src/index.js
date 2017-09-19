3ffecedfa5e738f85207935dafe9be4e4aa3c30b
an-art <anartyomchik@gmail.com>
2017-09-19 23:47:18 +0300

    count sum

--- src/index.js
+++ src/index.js
@@ -1,45 +1,55 @@
-module.exports = function multiply(first, second) {
+module.exports = function multiply(first, second) { 
 	var fir = first.split('').reverse().map( function(n) { 
-		return +n;
-	});
+	return +n; 
+	}); 
 	var sec = second.split('').reverse().map( function(n) { 
-		return +n;
-	});
-	var res = [];
-
-	for (var i=0; i<sec.length; i++) {
-		var prev_result = 0;
-		var fl = fir.length;
-		res[i] = fir.map( function(el) {
-			var mult = sec[i] * el;
-			var rest = mult % 10;
-			var result = Math.floor(mult / 10);
-			var sum = prev_result + rest;
-			fl--;
-			prev_result = result;
-			if (sum>=10 && fl>0) {
-				sum -= 10;
-				prev_result++;
-			}
-
-			return sum;
-		});
-	}
-
-	for (var i=0; i<res.length; i++) {
-		res.map( function(el) {
-			return el.unshift(0);
-		});
-		let x = 0;
-		let j = 0;
-		s[j] = 0;
-		while (x!=res[i].length) {
-			s[j] += res[i][x];
-			x++;
-			console.log(s);
+	return +n; 
+	}); 
+	var res = []; 
+	
+	for (var i=0; i<sec.length; i++) { 
+		var prev_result = 0; 
+		var fl = fir.length; 
+		res[i] = fir.map( function(el) { 
+			var mult = sec[i] * el; 
+			var rest = mult % 10; 
+			var result = Math.floor(mult / 10); 
+			var sum = prev_result + rest; 
+			fl--; 
+			prev_result = result; 
+			if (sum>=10 && fl>0) { 
+				sum -= 10; 
+				prev_result++; 
+			} 
+			return sum; 
+		}); 
+		var new_i = i; 
+		while (new_i!=0) { 
+			res[i].unshift(0); 
+			new_i--; 
+		} 
+		var rl = res[i].length;
+	} 
+	
+	var prod = []; 
+	var prev_res = 0;
+	var rl2 = rl;
+	for (var j=0; j<rl; j++) { 
+		prod[j] = 0;
+		for (var l=0; l<res.length; l++) { 
+			if(res[l][j]===undefined) res[l][j]=0; 
+			prod[j] += res[l][j];  
 		}
-		j++;
+		var rest = prod[j] % 10; 
+		var result = Math.floor(prod[j] / 10); 
+		prod[j] = prev_res + rest; 
+		rl2--; 
+		prev_res = result; 
+		if (prod[j]>=10 && rl2>0) { 
+			prod[j] -= 10; 
+			prev_res++; 
+		} 
+	} 
+	
+	return prod.reverse().join(''); 
 	}
-
-	return res;
-}
